1ff0affec1ddb3b4e835d5f976909888685aeb0b
Sam Halliday <sam.halliday@gmail.com>
2015-06-21 15:45:30 +0100

    all keys

--- create.sh
+++ create.sh
@@ -12,10 +12,12 @@
 #      images, before sending. Additional adhoc keys will be needed to
 #      complete a Kinesis layout. 105 [2] and 104 [3] layouts are
 #      available, I'm using 105 so that I get an extra R1 1.25.
+#      Most symbols discovered via [4].
 
 # [1] http://support.wasdkeyboards.com/customer/portal/articles/1366948-keycap-compatibility
 # [2] http://support.wasdkeyboards.com/customer/portal/kb_article_attachments/54760/original.svg
 # [3] http://support.wasdkeyboards.com/customer/portal/kb_article_attachments/56630/original.svg
+# [4] http://xahlee.info/comp/unicode_computing_symbols.html
 
 cat all.txt | while IFS=$'\t' read N A ; do
  cat example.svg | sed "saSYMBOLa${A}a" > symbols/$N.svg
@@ -31,3 +33,5 @@ done
 #cp special/tux.png symbols/67.png
 #cp special/tux.svg symbols/67.svg 
 #cp special/superman-inverted.svg symbols/67.svg
+
+cp special/superman-inverted.svg symbols/WIN_R5.svg 
